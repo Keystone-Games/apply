d19202dfc58831102cd2a1a3929566dcec8768cb
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-07 12:59:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function apply_fewd() {
 function apply_bewd() {
   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSf0xxMcmP_W1Jm_oHq9UWC5HiNRdofMRw9-tWykBeXegQUgIA/viewform";
 }
+function apply_ssfe() {
+  window.location.href = "https://forms.gle/ihjVyr1eScdMY8669";
+}
+function apply_ssbe() {
+  window.location.href = "";
+}
